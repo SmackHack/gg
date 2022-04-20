73a993f4ce4d260adb41699a107c9a18ce7df84c
SmackHack <88841044+SmackHack@users.noreply.github.com>
2022-04-20 15:00:15 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('.div1').effect( "slide",{direction: 'right'},2000)
+
+  $('.div2').effect( "slide",{},2000)
+
+});
+
+// $('.div1').animate({
+//       left: "-=100%"
+//   }, 3000);
+//   $('.div1').animate({
+//       opacity: '100'
+//   }, 15000);
